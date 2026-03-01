79c7cb69a5e8ef36ad351cc6110f599ee37e207f
Snehal Dangroshiya <snehal.dangroshiya@gmail.com>
2026-03-02 00:47:34 +0530

    feat(tabs): wire RequestTabs into App and remove request-store
    Add RequestTabs above RequestBuilder in the center panel and delete the
    old request-store.ts which has been superseded by tabs-store.ts.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RequestBuilder } from '@/components/request-builder/RequestBuilder'
+import { RequestTabs } from '@/components/request-builder/RequestTabs'
 import { CollectionsSidebar } from '@/components/collections/CollectionsSidebar'
 import { EnvironmentsPanel } from '@/components/collections/EnvironmentsPanel'
 import { ThemeProvider, useTheme } from 'next-themes'
@@ -105,7 +106,8 @@ function App() {
             
             {/* Center Panel - Request/Response (Bruno Style - Stacked) */}
             <main className="flex-1 flex flex-col min-w-0 bg-background">
-              <RequestBuilder 
+              <RequestTabs />
+              <RequestBuilder
                 onRequestSent={(req, res) => console.log('Request sent:', req, res)}
               />
             </main>
